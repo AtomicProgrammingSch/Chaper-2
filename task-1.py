cb94fbda7769f7dc0ae9a92839330dfede2aa9f7
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-01-25 16:18:55 +0000

    Finished task 1

---
+++ task-1.py
@@ -0,0 +1,16 @@
+length = 18
+width = 7
+
+
+def find_perimeter(l, w):
+    return (l * 2) + (w * 2)
+
+
+def find_area(l, w):
+    return l * w
+
+
+perimeter = find_perimeter(length, width)
+area = find_area(length, width)
+print("The perimeter is: {}".format(perimeter))
+print("The area is: {}".format(area))
