b93f90a30384393e2248e7cefb913eafc4608daf
AtomicProgrammingSch <george-timms@chace.enfield.sch.uk>
2021-01-27 12:28:04 +0000

    finished task-2

---
+++ task-2.py
@@ -0,0 +1,9 @@
+days_in_school_each_year = 192  # number of school days each year
+hours_in_school = 6  # number of hours spent in school each day
+
+# calculates the number of hours spent in school each year
+total_hours_in_school_year = days_in_school_each_year * hours_in_school
+
+total_hours_in_school = total_hours_in_school_year * 5  # 5 years, 7, 8, 9, 10, 11
+
+print("The total time spent in school is {} hours!".format(total_hours_in_school))
